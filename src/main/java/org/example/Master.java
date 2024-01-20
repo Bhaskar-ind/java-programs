8d9972e691b14bb4d18e71d551ffd2e054ba740d
Bhaskar Jaiswal <bhaskar.jaiswal126@gmail.com>
2024-01-20 16:07:15 +0530

    added java programs

---
+++ src/main/java/org/example/Master.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Master {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b = sc.nextInt();
+        int c =a+b;
+        System.out.print("Addition of both number:" +c);
+    }
+
+
+}
